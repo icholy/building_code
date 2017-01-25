d4d9933bc86aed971e752e130e0ec7a2ce38dfc4
Ilia Choly <ilia.choly@gmail.com>
2017-01-24 21:34:58 -0500

    start working on tagging

--- download.py
+++ download.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import ipdb
+import re
 from BeautifulSoup import BeautifulSoup
 
 url = "https://www.ontario.ca/laws/regulation/120332"
@@ -15,8 +16,12 @@ class_names = [
 ]
 out_file = "workspace/raw.json"
 
-def inner_html(e):
-    return "".join([str(x) for x in e.contents])
+def parse_element(e):
+    return {
+        "html":       "".join([str(x) for x in e.contents]),
+        "text":       e.text,
+        "class_name": e.attrMap["class"]
+    }
 
 def download():
     print("fetching {}".format(url))
@@ -24,14 +29,47 @@ def download():
     print("parsing html ...")
     soup = BeautifulSoup(resp.text)
     elements = soup("p", { "class": class_names })
-    lines = [ inner_html(e) for e in elements ]
-    print("writing {} lines to {} ...".format(len(lines), out_file))
+    entries = [ parse_element(e) for e in elements ]
+    print("writing {} entries to {} ...".format(len(entries), out_file))
     with open(out_file, "w") as f:
-        f.write(json.dumps(lines, indent = 2))
+        f.write(json.dumps(entries, indent = 2))
     print("done!")
 
+section_re    = re.compile(r"Section \d+\.\d+")
+article_re    = re.compile(r"\d+\.\d+\.\d+\.\d+")
+subsection_re = re.compile(r"\d+\.\d+\.\d+")
+sentence_re   = re.compile(r"\(\d+\)")
+clause_re     = re.compile(r"\([a-z]+(\.\d+)?\)")
+subclause_re  = re.compile(r"\([ivx]+(\.\d+)?\)")
+
+def get_tag(entry):
+    text = entry["text"]
+    class_name = entry["class_name"]
+    if section_re.match(text):
+        return "section"
+    if class_name == "section-e" and article_re.match(text):
+        return "article"
+    if class_name == "ruleb-e" and subsection_re.match(text):
+        return "subsection"
+    if sentence_re.match(text):
+        return "sentence"
+    if class_name == "clause-e" and clause_re.match(text):
+        return "clause"
+    if class_name == "subclause-e" and subclause_re.match(text):
+        return "subclause"
+    return "fragment"
+
+def tag_entries():
+    with open(out_file) as f:
+        entries = json.loads(f.read())
+        for entry in entries:
+            entry["tag"] = get_tag(entry)
+        with open("workspace/tagged.json", "w") as f:
+            tagged_json = json.dumps(entries, indent = 2)
+            f.write(tagged_json)
+
 def main():
-    download()
+    tag_entries()
 
 if __name__ == "__main__":
     main()
